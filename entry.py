309988f9cc00c66362210ae02dc69368b0c1472d
Picklegunner <rvs20hw@gmail.com>
2018-05-25 16:03:30 -0400

    Firebase upload added. Restructured entry as dictionary

--- entry.py
+++ entry.py
@@ -1,59 +1,23 @@
-VALID_UNITS = ("mg, g, N/A")
-
-class Nutrient:
-
-    def __init__(self, quantity=0, unit="N/A"):
-        self.set(quantity, unit)
-
-    def __str__(self):
-        return "{}{}".format(quantity, unit)
-
-    def set(self, quantity, unit):
-        self.quantity = quantity
-        if(not unit in VALID_UNITS):
-            message = "Invalid unit provided! {} is not one of these acceptable units: ".format(unit)
-            message += "".join(VALID_UNITS)
-            raise ValueError(message)
-        else:
-            self.unit = unit
-        return self
-
-    def get(self):
-        return [self.quantity, self.unit]
-
 class Entry:
 
     name = ""
 
-    calories = None
-    calories_from_fat = None
+    data = {
+        'calories' : None,
+        'calories_from_fat' : None,
 
-    total_fat = None
-    sat_fat = None
-    trans_fat = None
+        'total_fat' : {'unit': 'g', 'quantity': None},
+        'sat_fat' : {'unit': 'g', 'quantity': None},
+        'trans_fat' : {'unit': 'g', 'quantity': None},
 
-    total_carb = None
-    fiber = None
-    sugar = None
+        'total_carb' : {'unit': 'g', 'quantity': None},
+        'fiber' : {'unit': 'g', 'quantity': None},
+        'sugar' : {'unit': 'g', 'quantity': None},
 
-    cholesterol = None
-    protein = None
-    sodium = None
-
-    def __str__(self):
-        output = self.name + " - entry\n"
-        output += "Calories {}\n".format(self.calories)
-        output += "Calories from Fat {}\n".format(self.calories_from_fat)
-        output += "Total Fat {}\n".format(self.total_fat.get()[0])
-        output += "  Saturated Fat " + ''.join(self.sat_fat.get()) + "\n"
-        output += "  Trans. Fat " + ''.join(self.trans_fat.get()) + "\n"
-        output += "Total Carbs " + ''.join(self.total_carb.get()) + "\n"
-        output += "  Fiber " + ''.join(self.fiber.get()) + "\n"
-        output += "  Sugars " + ''.join(self.sugar.get()) + "\n"
-        output += "Cholesterol " + ''.join(self.cholesterol.get()) + "\n"
-        output += "Protein " + ''.join(self.protein.get()) + "\n"
-        output += "Sodium " + ''.join(self.sodium.get()) + "\n"
-        return output
+        'cholesterol' : {'unit': 'mg', 'quantity': None},
+        'protein' : {'unit': 'g', 'quantity': None},
+        'sodium' : {'unit': 'mg', 'quantity': None}
+    }
 
 
 
--- scrape.py
+++ scrape.py
@@ -4,6 +4,8 @@ from contextlib import closing
 from bs4 import BeautifulSoup
 import re
 from entry import *
+from firebase import firebase
+import json
 
 def simple_get(url):
     """
@@ -32,7 +34,7 @@ def is_good_response(resp):
             and content_type.find('html') > -1)
 
 #TODO create update_database that skims all available recipes and adds to sql database
-def scrape_recipe(url, name, location):
+def scrape_recipe(url, name):
 
     # Get html
     raw_html = simple_get(url)
@@ -57,30 +59,29 @@ def scrape_recipe(url, name, location):
             if stripped_element == "Serving Size":
                 pass
             elif stripped_element == "Total Fat":
-                scraped_entry.total_fat = Nutrient(re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip()), 'g')
+                scraped_entry.data['total_fat']['quantity'] = re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip())
             elif stripped_element == "Tot. Carb.":
-                scraped_entry.total_carb = Nutrient(re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip()), 'g')
+                scraped_entry.data['total_carb']['quantity'] = re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip())
             elif stripped_element == "Sat. Fat":
-                scraped_entry.sat_fat = Nutrient(re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip()), 'g')
+                scraped_entry.data['sat_fat']['quantity'] = re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip())
             elif stripped_element == "Dietary Fiber":
-                scraped_entry.fiber = Nutrient(re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip()), 'g')
+                scraped_entry.data['fiber']['quantity'] = re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip())
             elif stripped_element == "Trans Fat":
-                scraped_entry.trans_fat = Nutrient(re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip()), 'g')
+                scraped_entry.data['trans_fat']['quantity'] = re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip())
             elif stripped_element == "Sugars":
-                scraped_entry.sugar = Nutrient(re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip()), 'g')
+                scraped_entry.data['sugar']['quantity'] = re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip())
             elif stripped_element == "Cholesterol":
-                scraped_entry.cholesterol = Nutrient(re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip()), 'mg')
+                scraped_entry.data['cholesterol']['quantity'] = re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip())
             elif stripped_element == "Protein":
-                scraped_entry.protein = Nutrient(re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip()), 'g')
+                scraped_entry.data['protein']['quantity'] = re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip())
             elif stripped_element == "Sodium":
-                scraped_entry.sodium = Nutrient(re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip()), 'mg')
+                scraped_entry.data['sodium']['quantity'] = re.sub('[^0-9\\.]','', font_elements[i + 1].text.strip())
         elif cal_fat_search: # Calories from Fat
-            scraped_entry.calories_from_fat = cal_fat_search.group(1)
+            scraped_entry.data['calories_from_fat'] = cal_fat_search.group(1)
         elif cal_search: # Calories
-            scraped_entry.calories = cal_search.group(1)
+            scraped_entry.data['calories'] = cal_search.group(1)
 
     scraped_entry.name = name
-    scraped_entry.location = location
 
     return scraped_entry
 
@@ -106,11 +107,24 @@ def gather_links(url):
 
 def scrape_menu(url, location):
 
-    food_links = gather_links("This Week's Menus.htm") # TODO this function needs to gather menus by itself
+    fb = firebase.FirebaseApplication('https://nutriterp-database.firebaseio.com/', None)
 
+    food_links = gather_links("This Week's Menus.htm") # TODO this function needs to take menus as a param so multiple can be scraped
+    
     for link in food_links:
-        print(scrape_recipe(link['href'], link.text, location)) # TODO add to data structure
+        # TODO check if entry already exists before updating
+        entry = scrape_recipe(link['href'], link.text)
+        directory = "/Food/" + location + "/" + entry.name
+        result = fb.patch(directory, entry.data)
+
+
+def upload_to_firebase(fb, entry):
+    result = fb.get('/Food', None)
+    print(result)
 
 # TODO properly obtain location
 # TODO load all 3 menus for all 3 locations
+
+
 scrape_menu("This Week's Menus.htm", "southCampus")
+
